382502461a585151661db15827f778a3cc89ccb1
MOHAMEDBAKULY <BakulyBakulain@gmail.com>
2023-05-21 16:14:13 +0300

    commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let navbar = document.getElementById('navBar')
+let counting = document.getElementById('counting')
+
+const button = () => {
+    navbar.classList.toggle('hidemenu')
+}
+
+// counting.addEventListener('click', span )
+
+// const increment = () => {
+//     let count = 
+// }
